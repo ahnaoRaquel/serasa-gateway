177309cf5a29e0e286d97c9301779a6a31b266ab
Raquel Niehues <niehues.raquel@gmail.com>
2025-08-19 20:19:55 -0300

    Initial commit

---
+++ serasa-gateway/src/main/java/com/gateway/serasa/serasa_gateway/SerasaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.serasa.serasa_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerasaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerasaGatewayApplication.class, args);
+	}
+
+}
---
+++ serasa-gateway/src/test/java/com/gateway/serasa/serasa_gateway/SerasaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.serasa.serasa_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerasaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
